6c828479fb504305f66edf9267589cf383139828
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-01-10 20:02:23 +0300

    masalalar

---
+++ script.js
@@ -0,0 +1,35 @@
+// function multiplyNumeric() {
+//     for(let i in menu){
+
+//         if(typeof menu[i] == 'number'){
+//              console.log(menu[i] *= 2);
+//         }
+//     }
+// }
+// multiplyNumeric();
+
+
+// function getVoteCount(votes) {
+//     let res = 0;
+// 	for(let key in votes ){
+//       res =  votes.upvotes - votes.downvotes
+//     }
+//     return res;
+// }
+//  console.log(getVoteCount({ upvotes: 3, downvotes: 22 }))
+
+// getVoteCount({ upvotes: 13, downvotes: 0 })
+
+// getVoteCount({ upvotes: 2, downvotes: 33 }) ➞ -31
+
+// getVoteCount({ upvotes: 132, downvotes: 132 }) ➞ 0
+
+// function addition(num) {
+// 	return num + 1
+// }
+// console.log(addition(8));
+
+// function howManySeconds(hours) {
+// 	return (hours * 60)*60
+// }
+// console.log(howManySeconds(2));
